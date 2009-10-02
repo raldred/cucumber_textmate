dc6a59b565872bd2c6b742c58624acdfb7654e53
Rob Aldred <raldred@gmail.com>
2009-10-02 17:33:18 +0100

    rebuild to work with new hook api

--- textmate_formatter.rb
+++ textmate_formatter.rb
@@ -1,268 +1,251 @@
 require 'cucumber/formatter/ordered_xml_markup'
 require 'cucumber/formatter/duration'
 
-class TextmateFormatter < Cucumber::Ast::Visitor
+# module Cucumber
+#   module Formatter
+class TextmateFormatter
+  include ERB::Util # for the #h method
+  include Cucumber::Formatter::Duration
 
-    include ERB::Util # for the #h method
-    include Cucumber::Formatter::Duration
-    include Cucumber::Formatter
+  def initialize(step_mother, io, options)
+    @io = io
+    @step_mother = step_mother
+    @options = options
+    @buffer = {}
+    @builder = create_builder(@io)
+    @feature_number = 0
+    @scenario_number = 0
+    @step_number = 0
+    @header_red = nil
+  end
 
-    def initialize(step_mother, io, options)
-      super(step_mother)
-      @options = options
-      @builder = create_builder(io)
-      @feature_number = 0
-      @scenario_number = 0
-      @step_number = 0
-      @header_red = nil
+  def before_features(features)
+    @step_count = get_step_count(features)
+
+    # <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    @builder.declare!(
+      :DOCTYPE,
+      :html, 
+      :PUBLIC, 
+      '-//W3C//DTD XHTML 1.0 Strict//EN', 
+      'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
+    )
+
+    @builder << '<html xmlns ="http://www.w3.org/1999/xhtml">'
+      @builder.head do
+      @builder.meta(:content => 'text/html;charset=utf-8')
+      @builder.title 'Cucumber'
+      inline_css
+      inline_js
     end
-    
-    def create_builder(io)
-      OrderedXmlMarkup.new(:target => io, :indent => 0)
-    end
-    
-    def visit_features(features)
-      @step_count = get_step_count(features)
-      # <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-      @builder.declare!(
-        :DOCTYPE,
-        :html, 
-        :PUBLIC, 
-        '-//W3C//DTD XHTML 1.0 Strict//EN', 
-        'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
-      )
-      @builder.html(:xmlns => 'http://www.w3.org/1999/xhtml') do
-        @builder.head do
-          @builder.meta(:content => 'text/html;charset=utf-8')
-          @builder.title 'Cucumber'
-          inline_css
-          inline_js
-        end
-        @builder.body do
-          @builder << "<!-- Step count #{@step_count}-->"
-          @builder.div(:class => 'cucumber') do
-            @builder.div(:id => 'cucumber-header') do
-              @builder.div(:id => 'label') do
-                @builder.h1('Cucumber Features')
-              end
-              @builder.div(:id => 'summary') do
-                @builder.p('',:id => 'totals')
-                @builder.p('',:id => 'duration')
-              end
-            end
-            super
-          end
-          print_stats(features)
-        end
+    @builder << '<body>'
+    @builder << "<!-- Step count #{@step_count}-->"
+    @builder << '<div class="cucumber"'
+    @builder.div(:id => 'cucumber-header') do
+      @builder.div(:id => 'label') do
+        @builder.h1('Cucumber Features')
+      end
+      @builder.div(:id => 'summary') do
+        @builder.p('',:id => 'totals')
+        @builder.p('',:id => 'duration')
       end
     end
+  end
 
-    def visit_comment(comment)
-      @builder.pre(:class => 'comment') do
-        super
+  def after_features(features)
+    print_stats(features)
+    @builder << '</div>'
+    @builder << '</body>'
+    @builder << '</html>'
+  end
+
+  def before_feature(feature)
+    @exceptions = []
+    @builder << '<div class="feature">'
+  end
+
+  def after_feature(feature)
+    @builder << '</div>'
+  end
+
+  def before_feature_element(feature_element)
+    @scenario_number+=1
+    @scenario_red = false
+    css_class = {
+      Cucumber::Ast::Scenario        => 'scenario',
+      Cucumber::Ast::ScenarioOutline => 'scenario outline'
+    }[feature_element.class]      
+    @builder << "<div class='#{css_class}'>"
+  end
+
+  def after_feature_element(feature_element)
+    @builder << '</div>'
+    @open_step_list = true
+  end
+
+  def scenario_name(keyword, name, file_colon_line, source_indent)
+    @listing_background = false
+    @builder.h3(:id => "scenario_#{@scenario_number}") do
+      @builder.span(keyword, :class => 'keyword')
+      @builder.text!(' ')
+      @builder.span(name, :class => 'val')
+    end
+  end
+  
+  def before_outline_table(outline_table)
+    @outline_row = 0
+    @builder << '<table>'
+  end
+  
+  def after_outline_table(outline_table)
+    @builder << '</table>'
+    @outline_row = nil
+  end
+  
+  def before_steps(steps)
+    @builder << '<ol>'
+  end
+  
+  def after_steps(steps)
+    @builder << '</ol>'
+  end
+
+  def before_step(step)
+    @step_id = step.dom_id
+    @step_number += 1
+    @step = step
+  end
+
+  def after_step(step)
+    move_progress
+  end
+
+  def before_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
+    @step_match = step_match
+    if exception
+      return if @exceptions.index(exception)
+      @exceptions << exception
+    end
+    return if status != :failed && @in_background ^ background
+    @status = status      
+    set_scenario_color(status)      
+    @builder << "<li id='#{@step_id}' class='step #{status}'>"            
+  end
+
+  def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
+    # print snippet for undefined steps
+    if status == :undefined
+      step_multiline_class = @step.multiline_arg ? @step.multiline_arg.class : nil
+      @builder.pre do |pre|
+        pre << @step_mother.snippet_text(keyword,step_match.instance_variable_get("@name"),step_multiline_class)
       end
     end
+    @builder << '</li>'
+  end
 
-    def visit_comment_line(comment_line)
-      @builder.text!(comment_line)
-      @builder.br
+  def step_name(keyword, step_match, status, source_indent, background)
+    @step_matches ||= []
+    background_in_scenario = background && !@listing_background
+    @skip_step = @step_matches.index(step_match) || background_in_scenario
+    @step_matches << step_match
+
+    unless @skip_step
+      build_step(keyword, step_match, status)
     end
+  end
 
-    def visit_feature(feature)
-      @exceptions = []
-      @builder.div(:class => 'feature') do
-        super
-      end
+  def visit_exception(exception, status)
+    build_exception_detail(exception)
+  end
+
+  def extra_failure_content(file_colon_line)
+    @snippet_extractor ||= SnippetExtractor.new
+    "<pre class=\"ruby\"><code>#{@snippet_extractor.snippet(file_colon_line)}</code></pre>"
+  end
+
+  def before_multiline_arg(multiline_arg)
+    debugger
+    return if @hide_this_step || @skip_step
+    if Cucumber::Ast::Table === multiline_arg
+      @builder << '<table>'
     end
+  end
+  
+  def after_multiline_arg(multiline_arg)
+    return if @hide_this_step || @skip_step
+    if Cucumber::Ast::Table === multiline_arg
+      @builder << '</table>'
+    end
+  end
 
-    def visit_tags(tags)
+  def visit_py_string(string)
+    @builder.pre(:class => 'val') do |pre|
+      @builder << string.gsub("\n", '&#x000A;')
+    end
+  end
+
+  def visit_table_row(table_row)
+    @row_id = table_row.dom_id
+    @col_index = 0
+    @builder.tr(:class => 'step') do
       super
-      @tag_spacer = nil
     end
-
-    def visit_tag_name(tag_name)
-      @builder.text!(@tag_spacer) if @tag_spacer
-      @tag_spacer = ' '
-      @builder.span("@#{tag_name}", :class => 'tag')
-    end
-
-    def visit_feature_name(name)
-      lines = name.split(/\r?\n/)
-      return if lines.empty?
-      @builder.h2 do |h2|
-        @builder.span(lines[0], :class => 'val')
-      end
-      @builder.p(:class => 'narrative') do
-        lines[1..-1].each do |line|
-          @builder.text!(line.strip)
-          @builder.br
+    if table_row.exception
+      @builder.tr do
+        @builder.td(:colspan => @col_index.to_s,:class => 'step failed') do
+          build_exception_detail(table_row.exception)
         end
       end
     end
-
-    def visit_background(background)
-      @builder.div(:class => 'background') do
-        @in_background = true
-        super
-        @in_background = nil
-      end
+    if @outline_row
+      @outline_row += 1
     end
-
-    def visit_background_name(keyword, name, file_colon_line, source_indent)
-      @listing_background = true
-      @builder.h3 do |h3|
-        @builder.span(keyword, :class => 'keyword')
-        @builder.text!(' ')
-        @builder.span(name, :class => 'val')
-      end
-    end
-
-    def visit_feature_element(feature_element)
-      @scenario_number+=1
-      @scenario_red = false
-      css_class = {
-        Cucumber::Ast::Scenario        => 'scenario',
-        Cucumber::Ast::ScenarioOutline => 'scenario outline'
-      }[feature_element.class]
-      @builder.div(:class => css_class) do
-        super
-      end
-      @open_step_list = true
-    end
-    
-    def visit_scenario_name(keyword, name, file_colon_line, source_indent)
-      @listing_background = false
-      @builder.h3(:id => "scenario_#{@scenario_number}") do
-        @builder.span(keyword, :class => 'keyword')
-        @builder.text!(' ')
-        @builder.span(name, :class => 'val')
-      end
-    end
-
-    def visit_outline_table(outline_table)
-      @outline_row = 0
-      @builder.table do
-        super(outline_table)
-      end
-      @outline_row = nil
-    end
-
-    def visit_examples(examples)
-      @builder.div(:class => 'examples') do
-        super(examples)
-      end
-    end
-
-    def visit_examples_name(keyword, name)
-      @builder.h4 do
-        @builder.span(keyword, :class => 'keyword')
-        @builder.text!(' ')
-        @builder.span(name, :class => 'val')
-      end
-    end
-
-    def visit_steps(steps)
-      @builder.ol do
-        super
-      end
-    end
-
-    def visit_step(step)
-      @step_number += 1
-      @step_id = step.dom_id
-      super
-      move_progress
-    end
-
-    def visit_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
-      @step_match = step_match
-      if exception
-        return if @exceptions.index(exception)
-        @exceptions << exception
-      end
-      return if status != :failed && @in_background ^ background
-      @status = status
-      
-      set_scenario_color(status)
-      
-      @builder.li(:id => @step_id, :class => "step #{status}") do
-        super(keyword, step_match, multiline_arg, status, exception, source_indent, background)
-      end
-      
-    end
-
-    def visit_step_name(keyword, step_match, status, source_indent, background)
-      @step_matches ||= []
-      background_in_scenario = background && !@listing_background
-      @skip_step = @step_matches.index(step_match) || background_in_scenario
-      @step_matches << step_match
-
-      unless @skip_step
-        build_step(keyword, step_match, status)
-      end
-    end
-
-    def visit_exception(exception, status)
-      build_exception_detail(exception)
-    end
-    
-    def extra_failure_content(file_colon_line)
-      @snippet_extractor ||= SnippetExtractor.new
-      "<pre class=\"ruby\"><code>#{@snippet_extractor.snippet(file_colon_line)}</code></pre>"
-    end
-
-    def visit_multiline_arg(multiline_arg)
-      return if @skip_step
-      if Cucumber::Ast::Table === multiline_arg
-        @builder.table do
-          super
-        end
-      else
-        super
-      end
-    end
-
-    def visit_py_string(string)
-      @builder.pre(:class => 'val') do |pre|
-        @builder << string.gsub("\n", '&#x000A;')
-      end
-    end
-
-    def visit_table_row(table_row)
-      @row_id = table_row.dom_id
-      @col_index = 0
-      @builder.tr(:class => 'step') do
-        super
-      end
-      if table_row.exception
-        @builder.tr do
-          @builder.td(:colspan => @col_index.to_s,:class => 'step failed') do
-            build_exception_detail(table_row.exception)
+    @step_number += 1
+    move_progress
+  end
+  
+  
+  def before_table_row(table_row)
+    @row_id = table_row.dom_id
+    @col_index = 0
+    return if @hide_this_step
+    @builder << "<tr class='step' id='#{@row_id}'>"
+  end
+  
+  def after_table_row(table_row)
+    return if @hide_this_step
+    @builder << '</tr>'
+    if table_row.exception
+      @builder.tr do
+        @builder.td(:colspan => @col_index.to_s, :class => 'failed') do
+          @builder.pre do |pre|
+            pre << format_exception(table_row.exception)
           end
         end
       end
-      if @outline_row
-        @outline_row += 1
-      end
-      @step_number += 1
-      move_progress
     end
+    if @outline_row
+      @outline_row += 1
+    end
+    @step_number += 1
+    move_progress
+  end
 
-    def visit_table_cell_value(value, status)
-      @cell_type = @outline_row == 0 ? :th : :td
-      attributes = {:id => "#{@row_id}_#{@col_index}", :class => 'step'}
-      attributes[:class] += " #{status}" if status
-      build_cell(@cell_type, value, attributes)
-      set_scenario_color(status)
-      @col_index += 1
-    end
-    
-    def announce(announcement)
-      @builder.pre(announcement, :class => 'announcement')
-    end
+  def table_cell_value(value, status)
+    @cell_type = @outline_row == 0 ? :th : :td
+    attributes = {:id => "#{@row_id}_#{@col_index}", :class => 'step'}
+    attributes[:class] += " #{status}" if status
+    build_cell(@cell_type, value, attributes)
+    set_scenario_color(status)
+    @col_index += 1
+  end
 
-    protected
-    
+  def announce(announcement)
+    @builder.pre(announcement, :class => 'announcement')
+  end
+
+  protected
+
     def build_exception_detail(exception)
       backtrace = Array.new
       @builder.div(:class => 'message') do
@@ -283,7 +266,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
       extra = extra_failure_content(backtrace)
       @builder << extra unless extra == ""
     end
-    
+
     def set_scenario_color(status)
       if status == :undefined
         @builder.script do
@@ -300,7 +283,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
         end
       end
     end
-    
+
     def get_step_count(features)
       count = 0
       features = features.instance_variable_get("@features")
@@ -315,7 +298,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
           #get steps
           steps = scenario.instance_variable_get("@steps").instance_variable_get("@steps")
           count += steps.size
-          
+
           #get example table
           examples = scenario.instance_variable_get("@examples_array")
           unless examples.blank?
@@ -324,7 +307,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
               count += example_matrix.size
             end
           end
-          
+
           #get multiline step tables
           steps.each do |step|
             multi_arg = step.instance_variable_get("@multiline_arg")
@@ -336,7 +319,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
       end
       return count
     end
-    
+
     def build_step(keyword, step_match, status)
       step_name = step_match.format_args(lambda{|param| %{<span class="param">#{param}</span>}})
       @builder.div do |div|
@@ -347,7 +330,7 @@ class TextmateFormatter < Cucumber::Ast::Visitor
         end
       end
     end
-    
+
     def build_cell(cell_type, value, attributes)
       @builder.__send__(cell_type, attributes) do
         @builder.div do
@@ -361,30 +344,30 @@ class TextmateFormatter < Cucumber::Ast::Visitor
         @builder << File.read(File.dirname(__FILE__) + '/cucumber-textmate.css')
       end
     end
-    
+
     def inline_js
       @builder.script(:type => 'text/javascript') do
         @builder << inline_js_content
       end
     end
-    
+
     def inline_js_content
       <<-EOF
-function moveProgressBar(percentDone) {
+  function moveProgressBar(percentDone) {
   document.getElementById("cucumber-header").style.width = percentDone +"%";
-}
-function makeRed(element_id) {
+  }
+  function makeRed(element_id) {
   document.getElementById(element_id).style.background = '#C40D0D';
   document.getElementById(element_id).style.color = '#FFFFFF';
-}
+  }
 
-function makeYellow(element_id) {
+  function makeYellow(element_id) {
   document.getElementById(element_id).style.background = '#FAF834';
   document.getElementById(element_id).style.color = '#000000';
-}
-EOF
+  }
+  EOF
     end
-    
+
     def move_progress
       @builder << " <script type=\"text/javascript\">moveProgressBar('#{percent_done}');</script>"
     end
@@ -400,28 +383,27 @@ EOF
     def format_exception(exception)
       (["#{exception.message}"] + exception.backtrace).join("\n")
     end
-    
+
     def backtrace_line(line)
       line.gsub(/([^:]*\.(?:rb|feature|haml)):(\d*)/) do
         "<a href=\"txmt://open?url=file://#{File.expand_path($1)}&line=#{$2}\">#{$1}:#{$2}</a> "
       end
     end
-    
+
     def print_stats(features)
       @builder <<  "<script type=\"text/javascript\">document.getElementById('duration').innerHTML = \"Finished in <strong>#{format_duration(features.duration)} seconds</strong>\";</script>"
       @builder <<  "<script type=\"text/javascript\">document.getElementById('totals').innerHTML = \"#{print_stat_string(features)}\";</script>"
     end
-    
+
     def print_stat_string(features)
       string = String.new
-
-      string << dump_count(step_mother.scenarios.length, "scenario")
-      string << print_status_counts{|status| step_mother.scenarios(status)}
+      string << dump_count(@step_mother.scenarios.length, "scenario")
+      string << print_status_counts{|status| @step_mother.scenarios(status)}
       string << "<br />"
-      string << dump_count(step_mother.steps.length, "step")
-      string << print_status_counts{|status| step_mother.steps(status)}
+      string << dump_count(@step_mother.steps.length, "step")
+      string << print_status_counts{|status| @step_mother.steps(status)}
     end
-    
+
     def print_status_counts
       counts = [:failed, :skipped, :undefined, :pending, :passed].map do |status|
         elements = yield status
@@ -433,20 +415,23 @@ EOF
     def dump_count(count, what, state=nil)
       [count, state, "#{what}#{count == 1 ? '' : 's'}"].compact.join(" ")
     end
-    
+
+    def create_builder(io)
+      Cucumber::Formatter::OrderedXmlMarkup.new(:target => io, :indent => 0)
+    end
 end
 
 class SnippetExtractor #:nodoc:
   class NullConverter; def convert(code, pre); code; end; end #:nodoc:
   begin; require 'syntax/convertors/html'; @@converter = Syntax::Convertors::HTML.for_syntax "ruby"; rescue LoadError => e; @@converter = NullConverter.new; end
-  
+
   def snippet(error)
     raw_code, line = snippet_for(error[0])
     highlighted = @@converter.convert(raw_code, false)
     highlighted << "\n<span class=\"comment\"># gem install syntax to get syntax highlighting</span>" if @@converter.is_a?(NullConverter)
     post_process(highlighted, line)
   end
-  
+
   def snippet_for(error_line)
     if error_line =~ /(.*):(\d+)/
       file = $1
@@ -457,7 +442,7 @@ class SnippetExtractor #:nodoc:
       ["# Couldn't get snippet for #{error_line}", 1]
     end
   end
-  
+
   def lines_around(file, line)
     if File.file?(file)
       lines = File.open(file).read.split("\n")
@@ -470,7 +455,7 @@ class SnippetExtractor #:nodoc:
       "# Couldn't get snippet for #{file}"
     end
   end
-  
+
   def post_process(highlighted, offending_line)
     new_lines = []
     highlighted.split("\n").each_with_index do |line, i|
@@ -480,5 +465,5 @@ class SnippetExtractor #:nodoc:
     end
     new_lines.join("\n")
   end
-  
+
 end
